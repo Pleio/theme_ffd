7716ad2503bab4d44e059bdf87beefdd8ed4f733
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-08-25 10:10:13 +0200

    initial commit

---
+++ start.php
@@ -0,0 +1,16 @@
+<?php
+
+// define("THEME_GRAPHICS", elgg_get_site_url() . "mod/theme_ffd/_graphics/");
+// define("THEME_COLOR_1", "70bfb0"); // green
+// define("THEME_COLOR_2", "ffd500"); // yellow
+// define("THEME_COLOR_3", "f04e30"); // red
+// define("THEME_COLOR_4", "afb1ad"); // green
+// define("THEME_COLOR_5", "39a8d9"); // blue
+// define("THEME_COLOR_6", "2c2c2d"); // dark grey (text)
+// define("THEME_COLOR_7", "e5e5e5"); // light grey (background)
+
+function theme_ffd_init() {
+
+}
+
+elgg_register_event_handler('init', 'system', 'theme_ffd_init');
